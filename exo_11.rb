558f761d631827be80da5d5a9ed7a05f392fad5c
lifeae <romain.leporatti@laposte.net>
2020-01-14 13:37:24 +0100

    add exo_11.rb

---
+++ exo_11.rb
@@ -0,0 +1,4 @@
+#Écris un programme exo_11.rb qui demande un nombre à l'utilisateur, puis qui écrit autant de fois "Salut, ça farte ?"
+puts "Veuillez entrer un nombre :"
+nombre = gets.chomp.to_i
+puts "Salut, ça farte ?"
